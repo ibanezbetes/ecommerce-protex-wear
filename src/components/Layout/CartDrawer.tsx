469b4a01d9bb84d53a1c9109087b4f8068b40324
MarioCortes9 <duquegarciap@gmail.com>
2026-05-29 20:30:20 +0200

    fix(cart): remove apple pay and google pay express checkout buttons from cart drawer

--- src/components/Layout/CartDrawer.tsx
+++ src/components/Layout/CartDrawer.tsx
@@ -339,31 +339,7 @@ export function CartDrawer() {
 
         {items.length > 0 && (
           <div className={styles.footer}>
-            {/* Express Checkout buttons */}
-            <div className={styles.expressPayBlock}>
-              <div className={styles.expressPayTitle}>
-                <span className={styles.expressPayLine} />
-                <span className={styles.expressPayText}>Pago Rápido Express</span>
-                <span className={styles.expressPayLine} />
-              </div>
-              <div className={styles.expressPayButtons}>
-                <button className={styles.applePayBtn} onClick={() => handleExpressCheckout('Apple Pay')}>
-                  <svg width="16" height="16" viewBox="0 0 170 170" fill="currentColor" style={{ marginRight: '4px', position: 'relative', top: '-1px' }}>
-                    <path d="M150.37 130.25c-2.45 5.66-5.35 10.87-8.71 15.66-4.58 6.53-8.33 11.05-11.22 13.56-4.48 4.12-9.28 6.23-14.42 6.35-3.69 0-8.14-1.05-13.32-3.18-5.19-2.12-9.97-3.17-14.34-3.17-4.58 0-9.49 1.05-14.75 3.17-5.26 2.13-9.5 3.24-12.74 3.35-4.34.13-9.13-1.9-14.35-6.08-3.57-2.92-7.54-7.61-11.91-14.07-8.72-13.06-15.02-28.71-18.91-46.96-2.52-11.75-3.79-22.56-3.79-32.41 0-14.82 3.65-27.13 10.96-36.93 7.3-9.8 16.53-14.84 27.68-15.12 6.09 0 12.67 2.12 19.74 6.36 7.08 4.24 10.87 6.36 11.37 6.36.5 0 4.63-2.3 12.4-6.9 7.77-4.6 14.1-6.73 19.01-6.4 16.64.67 29.41 7.21 38.31 19.64-14.88 9.07-22.18 21.32-21.9 36.75.28 12.27 4.96 22.54 14.06 30.8 9.09 8.27 19.98 12.77 32.66 13.51-2.6 7.4-5.83 14.73-9.7 21.98zm-27.42-102.7c0-8.31 3-15.7 8.98-22.17 6-6.48 13.2-10.13 21.61-10.94.13 1.07.2 2.01.2 2.84 0 8.03-3.1 15.42-9.31 22.17-6.2 6.75-13.62 10.6-22.28 11.55-.4-2.15-.6-4.05-.6-6.45z"/>
-                  </svg>
-                  <span>Pay</span>
-                </button>
-                <button className={styles.googlePayBtn} onClick={() => handleExpressCheckout('Google Pay')}>
-                  <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles.googleIcon}>
-                    <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
-                    <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
-                    <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.06H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.94l2.85-2.22c-.87-2.6-2.6-4.53-4.16-4.53z" fill="#FBBC05"/>
-                    <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.06l3.66 2.84c.87-2.6 3.3-4.52 6.16-4.52z" fill="#EA4335"/>
-                  </svg>
-                  <span>Pay</span>
-                </button>
-              </div>
-            </div>
+
 
             {subtotal < 50 ? (
               <div className={styles.shippingBox}>
